5fa97d91ab834fba9f180a5aa022198452b12688
Himadri <himadris@gmail.com>
2020-08-30 11:36:04 -0700

    updated app.js and index.js in src folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route} from "react-router-dom";
 import './App.css';
+import NavBar from "./components/NavBar";
+import Header from "./components/Header";
+import NoMatch from "./pages/NoMatch";
+import Saved from "./pages/Saved";
+import Search from "./pages/Search";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar/>
+      <Header/>
+      <Switch>
+        <Route exact path ="/" component = {Search}/>
+        <Route exact path ="/search" component = {Search}/>
+        <Route exact path ="/saved" component = {Saved}/>
+        <Route exact path ="/noMatch" component = {NoMatch}/>
+      </Switch>
+      <Footer/>
+
+    </Router>
   );
 }
 
